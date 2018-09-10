4f5b9aa20788c5d696de5eb2f21a8e09fbd1d6f3
Ben <bas0041@auburn.edu>
2018-09-09 20:56:44 -0500

    Fix #2
    I couldn't pass command line arguments to run_script. This is intended to get around that. It creates a copy of 2_Run_SimFit and inserts a unique value of the seed to pass to 3_SimFit.R. Will test on HPC now

--- SimFitSRA/1_Run_Analysis.sh
+++ SimFitSRA/1_Run_Analysis.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
 # when ran on the HPC, include this to make R findable
-# source /opt/asn/etc/asn-bash-profiles-special/modules.sh
-# module load R/3.3.3
+source /opt/asn/etc/asn-bash-profiles-special/modules.sh
+module load R/3.3.3
 
-seeds=(1 2 3 4 5)
+# specify the seeds you wish to run
+# the length of this array specifies how many instances are initiated
+seeds=(100 200 300 400 500)
 
 echo "############################################"
 echo "####### Starting several jobs ##############"
 echo "############################################"
 
-# echo "Seeds to use: $seeds"
+echo " "
+echo "Seeds to use: ${seeds[@]}"
+echo " "
 
 for seed in ${seeds[@]}
 do
-  sh 2_Run_SimFit.sh $seed
-  # run_script 2_Run_SimFit.sh $seed
+  # create a copy of 2_Run_SimFit.sh with the third line changed to the $seed variable
+  awk 'NR==3 {$0="seed="'$seed'""} { print }' 2_Run_SimFit.sh > 2_Run_SimFit_$seed.sh
+  
+  # execute the temporary verison with specific seed
+  # sh 2_Run_SimFit_$seed.sh
+  run_script 2_Run_SimFit_$seed.sh
+  
+  # delete the temporary file
+  rm 2_Run_SimFit_$seed.sh
 done
-# wait
 
-echo "############################################"
-echo "Compiling Output"
-Rscript 4_CompileOutput.R
-echo "############################################"
-echo "Creating Plots"
-Rscript 5_MakePlots.R
-echo "############################################"
-echo "Zipping files"
-tar -cvzf Output.tar.gz Output
-echo "############################################"
-echo " "
-echo "Analysis done."
+# echo "############################################"
+# echo "Compiling Output"
+# Rscript 4_CompileOutput.R
+# echo "############################################"
+# echo "Creating Plots"
+# Rscript 5_MakePlots.R
+# echo "############################################"
+# echo "Zipping files"
+# tar -cvzf Output.tar.gz Output
+# echo "############################################"
+# echo " "
+# echo "Analysis done."
--- SimFitSRA/2_Run_SimFit.sh
+++ SimFitSRA/2_Run_SimFit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# seed goes here. will get placed here with awk when this file is copied
+
 # when ran on the HPC, include this to make R findable
 source /opt/asn/etc/asn-bash-profiles-special/modules.sh
 module load R/3.3.3
@@ -9,9 +11,9 @@ d=$(date)
 
 # $1 is the random seed provided by 1_Run_Analysis.sh
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "Running Iteration with Seed: $1"
+echo "Running Iteration with Seed: $seed"
 echo "  Started running at: $d"
-Rscript 3_SimFit.R $1
+Rscript 3_SimFit.R $seed
 
 # print the end date and time of this script
 d=$(date)
