45ed58d057dd090cd826e391593e78e69b8aa0a3
Ben <bas0041@auburn.edu>
2018-10-02 11:05:11 -0500

    update other scripts for tsm2
    compile output script
    bgr_summary script
    ess_summary script

--- SimFitSRA/4_CompileOutput.R
+++ SimFitSRA/4_CompileOutput.R
@@ -1,4 +1,13 @@
 # take the many separate .csv files and combine them into saved R objects
+extract_numbers = function(string, follow = NULL, as.num = F) {
+  x = unlist(regmatches(string, gregexpr(paste("[[:digit:]]+", follow, sep = ""), string)))
+  if (as.num) {
+    as.numeric(x)
+  } else {
+    x
+  }
+}
+
 
 # the main folder that houses the output
 out_folder = "Output"
@@ -12,21 +21,26 @@ first3 = substr(out_files, 1, 3)
 last3 = substr(out_files, nchar(out_files) - 2, nchar(out_files))
 lme_files = out_files[first3 == "lme" & last3 == "csv"]
 tsm_files = out_files[first3 == "tsm" & last3 == "csv"]
+tsm_1_files = tsm_files[substr(tsm_files, 5, 5) == 1]
+tsm_2_files = tsm_files[substr(tsm_files, 5, 5) == 2]
 param_files = out_files[first3 == "par" & last3 == "csv"]
 
 # the seeds that were saved for each type
-lme_seeds = unlist(as.numeric(regmatches(lme_files, gregexpr("[[:digit:]]+", lme_files))))
-tsm_seeds = unlist(as.numeric(regmatches(tsm_files, gregexpr("[[:digit:]]+", tsm_files))))
-param_seeds = unlist(as.numeric(regmatches(param_files, gregexpr("[[:digit:]]+", param_files))))
+lme_seeds = extract_numbers(lme_files, as.num = T)
+tsm_1_seeds = extract_numbers(extract_numbers(tsm_1_files, follow = ".csv"), as.num = T)
+tsm_2_seeds = extract_numbers(extract_numbers(tsm_2_files, follow = ".csv"), as.num = T)
+param_seeds = extract_numbers(param_files, as.num = T)
 
 # the seeds saved across all types
-seeds = unique(c(lme_seeds, tsm_seeds, param_seeds))
+seeds = sort(unique(c(lme_seeds, tsm_1_seeds, tsm_2_seeds, param_seeds)))
 n = length(seeds)
 
 # loop over these unique seeds:
 lme_summ = NULL
 param_summ = NULL
-tsm_summ = NULL
+tsm_1_summ = NULL
+tsm_2_summ = NULL
+
 for (i in 1:n) {
   
   if (seeds[i] %in% lme_seeds) {
@@ -39,14 +53,24 @@ for (i in 1:n) {
                          bgr = NA, ess = NA, iter = i)
   }
   
-  if (seeds[i] %in% tsm_seeds) {
-    tsm_tmp = read.csv(paste(out_dir, tsm_files[tsm_seeds == seeds[i]], sep = "/"), stringsAsFactors = F)
-    tsm_tmp$iter = i
+  if (seeds[i] %in% tsm_1_seeds) {
+    tsm_1_tmp = read.csv(paste(out_dir, tsm_1_files[tsm_1_seeds == seeds[i]], sep = "/"), stringsAsFactors = F)
+    tsm_1_tmp$iter = i
   } else {
-    tsm_tmp = data.frame(seed = seeds[i], 
-                         param = NA, stock = NA, method = "tsm",
+    tsm_1_tmp = data.frame(seed = seeds[i], 
+                         param = NA, stock = NA, method = "tsm1",
                          mean = NA, sd = NA, "X50." = NA, "X2.5." = NA, "X97.5." = NA, 
                          bgr = NA, ess = NA, iter = i)
+  }
+  
+  if (seeds[i] %in% tsm_2_seeds) {
+    tsm_2_tmp = read.csv(paste(out_dir, tsm_2_files[tsm_2_seeds == seeds[i]], sep = "/"), stringsAsFactors = F)
+    tsm_2_tmp$iter = i
+  } else {
+    tsm_2_tmp = data.frame(seed = seeds[i], 
+                           param = NA, stock = NA, method = "tsm1",
+                           mean = NA, sd = NA, "X50." = NA, "X2.5." = NA, "X97.5." = NA, 
+                           bgr = NA, ess = NA, iter = i)
   }
   
   if (seeds[i] %in% param_seeds) {
@@ -58,7 +82,8 @@ for (i in 1:n) {
   
   lme_summ = rbind(lme_summ, lme_tmp)
   param_summ = rbind(param_summ, param_tmp)
-  tsm_summ = rbind(tsm_summ, tsm_tmp)
+  tsm_1_summ = rbind(tsm_1_summ, tsm_1_tmp)
+  tsm_2_summ = rbind(tsm_2_summ, tsm_2_tmp)
 }
 
 # delete the intermediate files
@@ -69,4 +94,9 @@ for (i in 1:n) {
 # save the output as environment objects
 save(param_summ, file = paste(out_dir, "param_summ", sep = "/"))
 save(lme_summ, file = paste(out_dir, "lme_summ", sep = "/"))
-save(tsm_summ, file = paste(out_dir, "tsm_summ", sep = "/"))
+save(tsm_1_summ, file = paste(out_dir, "tsm_1_summ", sep = "/"))
+save(tsm_2_summ, file = paste(out_dir, "tsm_2_summ", sep = "/"))
+
+
+with(tsm_2_summ, boxplot(bgr ~ param, las = 3))
+
--- SimFitSRA/8_bgrSummary.R
+++ SimFitSRA/8_bgrSummary.R
@@ -1,17 +1,39 @@
 # script to summarize the BGR stats
 
-load("Output/tsm_summ")
-load("Output/lme_summ")
+# handle command line arguments: takes a single model
+args = commandArgs(trailingOnly = T)
+model = args[1]
+if (is.na(model)) {
+  stop("Must supply a model as a command line argument")
+}
 
+# load in the function needed to do this
 source("Functions/z9_bgr_summ_function.R")
 
-cat("\nTSM #1:")
-bgr_summ(summ = tsm_summ, vars = c("alpha", "beta", "U_msy", "S_msy", "U_MSY", "S_MSY", "U_obj", "S_obj"))
+if (model == "tsm1") {
+  load("Output/tsm_1_summ")
+  cat("\nTSM #1:")
+  bgr_summ(summ = tsm_1_summ, vars = c("alpha", "beta", "U_msy", "S_msy", "U_MSY", "S_MSY", "U_obj", "S_obj"))
+}
 
-cat("\nLM:")
-bgr_summ(summ = subset(lme_summ, method == "lm"), vars = c("alpha", "beta", "U_msy", "S_msy", "U_MSY", "S_MSY", "U_obj", "S_obj"))
+if (model == "tsm2") {
+  load("Output/tsm_2_summ")
+  cat("\nTSM #2:")
+  bgr_summ(summ = tsm_2_summ, vars = c("alpha", "beta", "mean_rho", "mean_sigma_R", "U_msy", "S_msy", "U_MSY", "S_MSY", "U_obj", "S_obj"))
+}
 
-cat("\nLME:")
-bgr_summ(summ = subset(lme_summ, method == "lme"), vars = c("alpha", "beta", "U_msy", "S_msy", "U_MSY", "S_MSY", "U_obj", "S_obj"))
+if (model == "lm") {
+  load("Output/lme_summ")
+  cat("\nLM:")
+  bgr_summ(summ = subset(lme_summ, method == "lm"), vars = c("alpha", "beta", "U_msy", "S_msy", "U_MSY", "S_MSY", "U_obj", "S_obj"))
+}
+
+
+if (model == "lme") {
+  load("Output/lme_summ")
+  cat("\nLME:")
+  bgr_summ(summ = subset(lme_summ, method == "lme"), vars = c("alpha", "beta", "U_msy", "S_msy", "U_MSY", "S_MSY", "U_obj", "S_obj"))
+}
+
 
 
