e025dfa537cf550294b4e80c56ce108fbcbcf781
Ben <bas0041@auburn.edu>
2018-10-03 09:35:11 -0500

    Update params summary
    Now monitoring the sigmas, mean sigma, and mean rho

--- SimFitSRA/Functions/7a_params_summary_function.R
+++ SimFitSRA/Functions/7a_params_summary_function.R
@@ -1,14 +1,21 @@
 # attach(params)
 
+attach(params)
 params_summary = function(params, seed) {
   
   output = with(params, {
+    # get mean rho
+    rho_mat2 = rho_mat
+    diag(rho_mat2) = NA
+    mean_rho = mean(rho_mat2, na.rm = T)
     
-    p = c(rep("alpha", ns), rep("beta", ns), rep("U_msy", ns), rep("S_msy", ns), "S_obj", "U_obj", "U_MSY", "S_MSY")
-    s = c(rep(1:ns, 4), rep(NA, 4))
-    v = c(alpha, beta, U_msy, S_msy)
+    p = c(rep("alpha", ns), rep("beta", ns), rep("sigma_R", ns), rep("U_msy", ns),
+          rep("S_msy", ns), "mean_rho", "mean_sigma_R", "S_obj", "U_obj", "U_MSY", "S_MSY")
+    s = c(rep(1:ns, 5), rep(NA, 6))
+    v = c(alpha, beta, sigma, U_msy, S_msy, mean_rho, mean(sigma))
     dw = unname(gen_mgmt(params = params)$mgmt[c("S_obj", "U_obj", "U_MSY", "S_MSY")])
     v = c(v, dw)
+    
     
     data.frame(seed = seed, stock = s, param = p, value = v)
     
@@ -17,3 +24,5 @@ params_summary = function(params, seed) {
   # return output
   return(output)
 }
+
+names(params)
